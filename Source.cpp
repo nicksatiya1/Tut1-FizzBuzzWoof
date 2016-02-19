f6d59fe9e074878c1be4a4103dda0bde5997b0a8
nicksatiya1 <nicksatiya1@gmail.com>
2016-02-19 12:17:57 +0200

    FizzBuzzWoof

--- Source.cpp
+++ Source.cpp
@@ -3,15 +3,26 @@ using namespace std;
 
 int main(){
 	int i = 1;
-	while (i <= 20)
+	while (i <= 50)
 	{
 
-		if (i % 3 != 0 && i % 5 != 0){
+		if (i % 3 != 0 && i % 5 != 0 && i %7!=0){
 			cout << i << endl;
+		}
+		else if (i % 3 == 0 && i % 5 == 0 && i&7==0){
+			cout << "FizzBuzzWoof" << endl;
 		}
 
 		else if (i % 3 == 0 && i % 5 == 0){
 			cout << "FizzBuzz" << endl;
+		}
+
+		else if (i % 3 == 0 && i % 7 == 0){
+			cout << "FizzWoof" << endl;
+		}
+
+		else if (i % 5 == 0 && i % 7 == 0){
+			cout << "BuzzWoof" << endl;
 		}
 		else if (i % 5 == 0){
 			cout << "Fizz" << endl;
@@ -20,6 +31,9 @@ int main(){
 			cout << "Buzz" << endl;
 		}
 
+		else if (i % 7 == 0){
+			cout << "Woof" << endl;
+		}
 		i++;
 	}
 }
